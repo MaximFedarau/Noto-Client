36c20d7ea45352a58803468af607d10eae9fe41b
maxim <fedaraumaxim@gmail.com>
2022-08-10 18:06:51 +0300

    fix: fix for PR#8 Search Bar update: it scales maximum + some horizontal padding.

--- components/Default/SearchBar/SearchBar.component.tsx
+++ components/Default/SearchBar/SearchBar.component.tsx
@@ -5,19 +5,22 @@ import {
   useWindowDimensions,
   Platform,
 } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { SearchBarInput } from './SearchBar.styles';
 
 export default function SearchBar(props: TextInputProps): ReactElement {
   const { width } = useWindowDimensions();
   const { OS } = Platform;
+  const insets = useSafeAreaInsets();
   return (
     <View
       style={{
-        width: width > 600 ? 500 : 240,
-        height: 40,
+        width: width - 112,
         alignSelf: 'center',
-        marginTop: OS === 'ios' && width > 600 ? 4 : 0,
+        height: 36,
+        paddingRight: insets.right,
+        paddingLeft: insets.left,
       }}
     >
       <SearchBarInput
